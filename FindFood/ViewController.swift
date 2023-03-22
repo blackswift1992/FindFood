9f34e2f1bf9788f251a05645e1b625d94e667f45
Oleksii Moroz <blackswift1992@gmail.com>
2023-03-23 00:05:40 +0200

    add imagePicker object, set up it and chain it with cameraTapped IBAction

--- FindFood/ViewController.swift
+++ FindFood/ViewController.swift
@@ -11,16 +11,21 @@ import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    private let imagePicker = UIImagePickerController()
     
-    @IBOutlet weak var cameraImageView: UIImageView!
+    @IBOutlet private weak var cameraImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
         
     }
 
-    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+    @IBAction private func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker , animated: true)
     }
     
 }
